ba4ca970e0de6a6934157e58866a823ebb1ed720
Karla R <karla.y.reyes12@gmail.com>
2018-08-02 10:52:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,8 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  DB = {:conn => SQLite3::Database.new("db/students.db")}
+  
+  
+  
 end
